ed94fe5fea3baad447e0a2068abfd90ebe226bf7
Jason Warner <jcwarner@gmail.com>
2018-03-04 20:08:26 -0600

    Remove start button after click, display new question & answers from array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,11 +2,19 @@ $(document).ready( function() {
 
     var triviaQueryURL = "https://opentdb.com/api.php?amount=10&category=21&type=multiple";
     var questionArray = [];
+    var numQuestions = 0;
+    var curQuestion = 0;
+
+    var newAnswerRowStart = "<div class='row'><button class='btn btn-default poss-answer'>";
 
     // New Game function to GET Trivia Question Object from Open Trivia Database API
     // Category "Sports", Quantity "10", Type "Multiple Choice", Difficulty "Any"
     // Store response results into questionArray
     function newGame() {
+
+        // remove start game button
+        $(".btn-primary").toggle();
+
         $.ajax({
             url: triviaQueryURL,
             method: "GET"
@@ -15,10 +23,23 @@ $(document).ready( function() {
                 questionArray.push(response.results[i]);
                 console.log(questionArray[i]);
             }
+
+            numQuestions = questionArray.length;
+            console.log(numQuestions);
         });
     };
 
     // Display next question and possible answers from questionArray
+    function getQuestion() {
+        $("#question").text(questionArray[curQuestion].question);
+
+        // display possible answers randomly - currently not random
+        $(".answers").append(newAnswerRowStart + questionArray[curQuestion].correct_answer + "</button></div>");
+        $(".answers").append(newAnswerRowStart + questionArray[curQuestion].incorrect_answers[0] + "</button></div>");
+        $(".answers").append(newAnswerRowStart + questionArray[curQuestion].incorrect_answers[1] + "</button></div>");
+        $(".answers").append(newAnswerRowStart + questionArray[curQuestion].incorrect_answers[2] + "</button></div>");
+
+    }
     // x amount of time to answer question
     // continually store correct guesses and wrong guesses
     // if correct, then show congratulation screen, wait few seconds, then run getQuestion
@@ -34,7 +55,7 @@ $(document).ready( function() {
         // generate question array
         newGame(); 
         // get and display next question after question array successfully retrieved
-        getQuestion();
+        setTimeout( getQuestion, 1000);
     });   
 
     
