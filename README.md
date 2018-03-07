cb295ffa7a1e58997a1e73bbb3df5f10cc829677
Jason Warner <jcwarner@gmail.com>
2018-03-06 23:33:40 -0600

    Semi-final version to submit a working game theory, minus a few requirements

--- README.md
+++ README.md
@@ -23,5 +23,3 @@ style.css & Bootstrap
 
 ## JavaScript & jQuery
 app.js
-
-* BE SURE TO REMOVE ALL CONSOLE.LOGS PRIOR TO SUBMITTING FINAL APP VERSION
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,14 +3,14 @@ $(document).ready( function() {
     var triviaQueryURL = "https://opentdb.com/api.php?amount=10&category=21&type=multiple";
     var questionArray = [];
     var possibleAnswers = [];
-    var randString = ""
-    var randNum = 0;
+    var currentQuestion = "";
     var numQuestions = 0;
     var curQuestion = 0;
     var correctGuesses = 0;
     var wrongGuesses = 0;
+    var countDown;
 
-    var newAnswerRowStart = "<div class='row poss-answer'><button class='btn btn-default'>";
+    var newAnswerRowStart = "<div class='row poss-answer'><h2>";
 
     // New Game function to GET Trivia Question Object from Open Trivia Database API
     // Category "Sports", Quantity "10", Type "Multiple Choice", Difficulty "Any"
@@ -19,21 +19,17 @@ $(document).ready( function() {
 
         questionArray = [];
         possibleAnswers = [];
-        randString= "";
-        randNum = 0;
+        currentQuestion = "";
         numQuestions = 0;
         curQuestion = 0;
         correctGuesses = 0;
         wrongGuesses = 0;
+        countDown;
 
         $(".row-question").toggle();
         $(".row-answers").toggle();
-
-        // remove start game button
-        $(".btn-primary").toggle();
-        // remove previous game results
-        $(".guesses").remove();
-
+        $(".btn-success").toggle();
+        
         $.ajax({
             url: triviaQueryURL,
             method: "GET"
@@ -44,95 +40,147 @@ $(document).ready( function() {
 
             numQuestions = questionArray.length;
 
-            // get and display next question after question array successfully retrieved
             getQuestion();
             
         });
     };
 
-    // if correct, then show congratulation screen, wait few seconds, then run getQuestion
-    // if wrong, then show wrong answer screen & correct answer, wait a few seconds, then run getQuestion
-    // if time runs out, then show screen time ran out & correct answer, wait a few seconds, then run getQuestion
-    // After all questions exhausted
-    // display correct and wrong guesses
-    // give option to restart game, if clicked, run newGame
 
+    // -------------------
+    // If, current question count is less than number of questions in array, then
+    // Retrieve current question and correct answer from question array
+    // Retrieve possible answers from current question from question array
+    // Randomize answer order in possible answers array
+    // Run displayQuestion
+    // -------------------
+    // Else, run endGame as all questions exhausted
     function getQuestion() {
+
+        possibleAnswers = [];
 
         if (curQuestion < numQuestions) {
 
+            currentQuestion = questionArray[curQuestion].question;
+            console.log( questionArray[curQuestion].correct_answer);
             possibleAnswers.push(questionArray[curQuestion].correct_answer);
 
             for (var j = 0; j < 3; j++) {
                 possibleAnswers.push(questionArray[curQuestion].incorrect_answers[j]);
-            }            
+            } 
     
+            /**
+             * Randomize array element order in-place.
+             * Using Durstenfeld shuffle algorithm.
+             */            
+            for (var i = possibleAnswers.length - 1; i > 0; i--) {
+                var j = Math.floor(Math.random() * (i + 1));
+                var temp = possibleAnswers[i];
+                possibleAnswers[i] = possibleAnswers[j];
+                possibleAnswers[j] = temp;
+            }
+
             displayQuestion();
 
         } else {
-            $(".row-question").toggle();
-            $(".row-answers").toggle();
-            $(".btn-primary").toggle();
-            $(".row-results").toggle();
-            $(".row-results").append("<div class='guesses'><h2>" + "Correct Guesses: " + correctGuesses + "</h2></div>");
-            $(".row-results").append("<div class='guesses'><h2>" + "Wrong Guesses: " + wrongGuesses + "</h2></div>");
+            endGame();
         }
 
     };
 
+    // Display results of game after all questions exhausted
+    function endGame () {
+        $(".row-question").toggle();
+        $(".row-answers").toggle();
+        $(".btn-success").toggle();
+        $(".row-results").toggle();
+        $(".count-down").html("");
+        $(".row-results").append("<div class='guesses'><h2>" + "Correct Guesses: " + correctGuesses + "</h2></div>");
+        $(".row-results").append("<div class='guesses'><h2>" + "Wrong Guesses: " + wrongGuesses + "</h2></div>");
+    }
+
+
+    // Display current question and answers
+    // After display, call listenAnswer and timeAnswer
     function displayQuestion() {
 
-        /**
-         * Randomize array element order in-place.
-         * Using Durstenfeld shuffle algorithm.
-         */            
-        for (var i = possibleAnswers.length - 1; i > 0; i--) {
-            var j = Math.floor(Math.random() * (i + 1));
-            var temp = possibleAnswers[i];
-            possibleAnswers[i] = possibleAnswers[j];
-            possibleAnswers[j] = temp;
-        }
+        $("#question").html(currentQuestion);
+        $(".col-answers").append(newAnswerRowStart + possibleAnswers[0] + "</h2></div>");
+        $(".col-answers").append(newAnswerRowStart + possibleAnswers[1] + "</h2></div>");
+        $(".col-answers").append(newAnswerRowStart + possibleAnswers[2] + "</h2></div>");
+        $(".col-answers").append(newAnswerRowStart + possibleAnswers[3] + "</h2></div>");
 
-        $("#question").html(questionArray[curQuestion].question);
-
-        // display possible answers randomly - currently not random
-        $(".col-answers").append(newAnswerRowStart + possibleAnswers[0] + "</button></div>");
-        $(".col-answers").append(newAnswerRowStart + possibleAnswers[1] + "</button></div>");
-        $(".col-answers").append(newAnswerRowStart + possibleAnswers[2] + "</button></div>");
-        $(".col-answers").append(newAnswerRowStart + possibleAnswers[3] + "</button></div>");
-
+        timeAnswer();
         listenAnswer();
+        
 
     };
 
+    // Interval function that gives user set amount of time to answer questions
+    function timeAnswer() {
+
+        var setTime = 30;
+        $(".count-down").html(setTime);
+
+        countDown = setInterval(gameTime, 1000);
+
+        function gameTime() {
+
+            if (setTime === 0) {
+                clearInterval(countDown);
+                ranOutofTime();
+            } else {
+                setTime--;
+            }
+            $(".count-down").html(setTime);
+        }        
+    }
+
+    // Alert user they ran out of time, get next question
+    function ranOutofTime() {        
+        alert("Time Ran Out!!");
+        curQuestion++;
+        wrongGuesses++;
+        $("#question").html("");
+        $(".poss-answer").remove();
+        getQuestion();
+    }
+
+    function rightAnswer() {                        
+        alert("Correct Guess!!");
+        curQuestion++;
+        correctGuesses++;
+        $("#question").html("");
+        $(".poss-answer").remove();
+        getQuestion();
+    }
+
+    function wrongAnswer() {
+        alert("Wrong Guess!!");
+        curQuestion++;
+        wrongGuesses++;
+        $("#question").html("");
+        $(".poss-answer").remove();
+        getQuestion();
+    }
+
+    // Listen for answer guess
     function listenAnswer() {
 
-        // begin clock countdown as questions and answers are displayed
-        // need to incapsulate this in a function
-            
-        $(".btn-default").on("click", function() {
+        $(".poss-answer").on("click", function()  {
 
-            if (this.innerText === questionArray[curQuestion].correct_answer) {
-                alert("You Won");
-                curQuestion++;
-                correctGuesses++;
-                $("#question").html("");
-                $(".poss-answer").remove();
-                getQuestion();
+            clearInterval(countDown);
+
+            if (this.innerText ===  questionArray[curQuestion].correct_answer) {
+                rightAnswer();
             } else {
-                alert("You Lost");
-                curQuestion++;
-                wrongGuesses++;
-                $("#question").html("");
-                $(".poss-answer").remove();
-                getQuestion();
-            }
+                wrongAnswer();
+            }    
         });
 
     };
 
     // On click listener for start game
-    $(".btn-primary").on("click", function() {
+    $(".btn-success").on("click", function() {
         // generate question array
         newGame(); 
     });       
